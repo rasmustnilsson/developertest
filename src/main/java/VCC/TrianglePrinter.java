90b361935e85caaf953ec0cdc2b7fa9f12d7ff2f
Rasmus Nilsson <rasmustnilsson@outlook.com>
2021-10-28 20:44:47 +0200

    Initial commit

---
+++ src/main/java/VCC/TrianglePrinter.java
@@ -0,0 +1,9 @@
+package VCC;
+
+public class TrianglePrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/VCC/TrianglePrinterTest.java
@@ -0,0 +1,14 @@
+package VCC;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TrianglePrinterTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
